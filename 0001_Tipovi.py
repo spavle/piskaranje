0bfbd693ce6f87695bdecb27c1a3d65fb3b224d1
spavle <spavle@users.noreply.github.com>
2015-01-19 12:50:16 +0100

    Tipovi varijabli
    Fajla sa tipovima varijabli

---
+++ 0001_Tipovi.py
@@ -0,0 +1,15 @@
+# Komentar, hash i jedan razmak
+
+# Digits
+
+cijeli_broj = 2
+decimalni_broj = 3.37
+
+# dynamically typed, variable can reference any type of object
+# variables are only reference to objects in memory
+
+nesto = 1   # integer
+print 'nesto' , nesto , type ( nesto )
+
+nesto = 2.456  # decimal
+print 'nesto' , nesto , type ( nesto )
