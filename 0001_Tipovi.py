70c589732421b9f6f95aff361cf3fe072434eed6
spavle <spavle@users.noreply.github.com>
2015-01-19 13:11:37 +0100

    rad na tipovima

--- 0001_Tipovi.py
+++ 0001_Tipovi.py
@@ -13,3 +13,18 @@ print 'nesto' , nesto , type ( nesto )
 
 nesto = 2.456  # decimal
 print 'nesto' , nesto , type ( nesto )
+
+# "Tipovi" su built in objects
+
+# Numbers
+
+a = 5
+a = 3.14
+a = 3+5j
+a = 0b1001
+
+# strings
+
+a = "Bla"	# ide i " i ' kao odrednica stringova
+a = 'Bli'
+
