1fddc5c639d15363f5de6eace4d079daff99ade5
spavle <spavle@users.noreply.github.com>
2015-07-16 16:07:51 +0200

    Manje dorade

--- piramida.py
+++ piramida.py
@@ -3,7 +3,7 @@
 from mc import * #import api-ja
 mc = Minecraft() #inicijalizacija sustava za rad sa Minecraftom
 
-def chep ():
+def piramida ():
    """
    funkcija za crtanje chepa od pjeska dimenzija 9x3x3 neposredno ispred lika
    """
@@ -16,18 +16,23 @@ def chep ():
    mc.setBlock(radnaPozicija.x  , radnaPozicija.y  , radnaPozicija.z , GOLD_BLOCK)
    while 1 :		# ne pod 45
       sirina += 1
+      Blok = SANDSTONE
+      #QUARTZ_BLOCK
+      if (sirina % 12345) == 0 :     #placeholder
+         Blok = STONE
+      elif (sirina % 5 ) == 0  :     # svaki peti red
+         Blok = SANDSTONE
+         #DIAMOND_BLOCK
       for dZ in  range( -sirina , sirina +1) :    		# prodji chep
-         mc.setBlock(radnaPozicija.x + sirina , radnaPozicija.y - sirina , radnaPozicija.z + dZ , DIAMOND_BLOCK)			#postavi blok   
-         #mc.setBlock(radnaPozicija.x + sirina , radnaPozicija.y - sirina , radnaPozicija.z - dZ , DIAMOND_BLOCK)			#postavi blok   
-         mc.setBlock(radnaPozicija.x - sirina , radnaPozicija.y - sirina , radnaPozicija.z + dZ , DIAMOND_BLOCK)			#postavi blok   
-         #mc.setBlock(radnaPozicija.x - sirina , radnaPozicija.y - sirina , radnaPozicija.z - dZ, DIAMOND_BLOCK)			#postavi blok   
-         mc.setBlock(radnaPozicija.x + dZ , radnaPozicija.y - sirina , radnaPozicija.z + sirina , DIAMOND_BLOCK)			#postavi blok   
-         mc.setBlock(radnaPozicija.x - dZ , radnaPozicija.y - sirina , radnaPozicija.z - sirina , DIAMOND_BLOCK)			#postavi blok   
-         if mc.getBlock ( radnaPozicija.x , radnaPozicija.y - sirina , radnaPozicija.z ) == BEDROCK.id :
+         mc.setBlock(radnaPozicija.x + sirina , radnaPozicija.y - sirina , radnaPozicija.z + dZ , Blok)			#postavi blok   
+         mc.setBlock(radnaPozicija.x - sirina , radnaPozicija.y - sirina , radnaPozicija.z + dZ , Blok)			#postavi blok   
+         mc.setBlock(radnaPozicija.x + dZ , radnaPozicija.y - sirina , radnaPozicija.z + sirina , Blok)			#postavi blok   
+         mc.setBlock(radnaPozicija.x - dZ , radnaPozicija.y - sirina , radnaPozicija.z - sirina , Blok)			#postavi blok   
+         if mc.getBlock ( radnaPozicija.x , radnaPozicija.y - sirina , radnaPozicija.z ) == BEDROCK.id :                # kad dodje do podloge zavrsi
             return 1
    return 1
    
-chep ()
+piramida ()
 
 
          
