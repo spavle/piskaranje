6f49c2b254a8bff6b41d3a2978464fbbb4e986b7
spavle <spavle@users.noreply.github.com>
2015-09-15 16:08:05 +0200

    alat za runtime crtanje
    ništa bitno :abcd:

---
+++ ploha.py
@@ -0,0 +1,16 @@
+#definicija objekta i poziv rutine za crtanje
+from crtanje import *		#tu je funkcija koju zovem
+from crtanje2 import *		#tu je funkcija koju zovem
+from mc import *			
+
+mc = Minecraft() #inicijalizacija sustava za rad sa Minecraftom
+
+crtaj_kvadar ( gdjeSam () , [ 2 , 0 , 0 ]  , [ 2 , 7 , 4  ] , gdjeGledam () , 5 , 0 )
+#crtaj_kvadar ( gdjeSam () , [ 2 , -3 , -1 ]  , [ 2 , 30 , 4  ] , gdjeGledam () , 5 , 0 )
+
+"""
+podloga stepenica
+for dd in range ( 0 , 6 ) :
+   crtaj_kvadar ( gdjeSam () , [ 1 , - dd , dd ]  , [ 13 , -dd , dd  ] , gdjeGledam () , 5 , 0 )
+
+"""
